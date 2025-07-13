59365587f41ced455caa16a8c20bd25f62def430
Gabriel Souza <gabrielpaiva423@gmail.com>
2025-07-13 16:18:25 -0300

    feat: implement audio upload and transcription, enhance question creation with embeddings

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastifyCors from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import fastify from 'fastify'
 import {
   serializerCompiler,
@@ -9,12 +10,15 @@ import { createQuestionRoute } from './http/routes/create-question.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -27,6 +31,7 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   // biome-ignore lint/suspicious/noConsole: I like it :)
